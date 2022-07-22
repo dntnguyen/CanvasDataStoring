1852c16e01b7f366c099456889dc38190c44eb13
dntnguyen <33388170+dntnguyen@users.noreply.github.com>
2022-07-22 11:35:20 +0700

    clone a new net framework 4.6.1 project

---
+++ CanvasDataDemoNetFramework/CanvasDataDemoNetFramework/DatabaseHelper/MyConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanvasDataDemoNetFramework.DatabaseHelper
+{
+    public static class MyConnection
+    {
+        public const int DEFAULT_QUERY_TIMEOUT_IN_SECOND = 30;
+
+        public static string GlobalConnectionString = "";
+
+        public static void SetGlobalConnectionString(string connectionString)
+        {
+            GlobalConnectionString = connectionString;
+        }
+    }
+}
